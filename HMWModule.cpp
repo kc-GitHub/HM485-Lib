76a63f27fe8b7beed8f7391d958e789cb0d650a6
ThorstenPferdekaemper <thorsten@pferdekaemper.com>
2014-07-10 22:38:33 +0200

    Die CCU liest beim Pairing jetzt alle Einstellungen
    ...und nicht nur die erste Hälfte.
    Die E/e-Messages sind jetzt etwas klarer

--- HMWModule.cpp
+++ HMWModule.cpp
@@ -80,21 +80,22 @@ void HMWModule::processEvents() {
             // TODO: Der E-Befehl wird von der Zentrale beim Pairing geschickt
         	//       ganz klar ist aber nicht, was er macht. Bisher kam als
         	//       Daten immer 0x00001040 mit und das wurde von einem echten
-        	//       Device mit einem e und 00 00 10 1F 00 00 00 00 00 60 00 beantwortet
-        	//       Einfach mal nachmachen, aber 100% richtig ist das wahrscheinlich nicht
+        	//       Device mit einem e und 00 00 10 xx xx xx xx xx xx xx xx beantwortet.
+        	//       Die x stehen fuer ein Bitmuster. Ein gesetztes Bit bedeutet, dass
+        	//       die entsprechenden 16 Byte im EEPROM benutzt sind.
         	sendAck = 0;
         	hmwrs485->txFrameDataLength = 12;
         	hmwrs485->txFrameData[0] = 0x65;  //e
         	hmwrs485->txFrameData[1] = 0x00;
         	hmwrs485->txFrameData[2] = 0x00;
         	hmwrs485->txFrameData[3] = 0x10;
-        	hmwrs485->txFrameData[4] = 0x1F;
-        	hmwrs485->txFrameData[5] = 0x00;
+        	hmwrs485->txFrameData[4] = 0xFF;
+        	hmwrs485->txFrameData[5] = 0x07;
         	hmwrs485->txFrameData[6] = 0x00;
         	hmwrs485->txFrameData[7] = 0x00;
         	hmwrs485->txFrameData[8] = 0x00;
         	hmwrs485->txFrameData[9] = 0x00;
-        	hmwrs485->txFrameData[10] = 0x60;
+        	hmwrs485->txFrameData[10] = 0x00;
         	hmwrs485->txFrameData[11] = 0x00;
             break;
          case 'K':                                                              // Key-Event
